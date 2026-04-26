0e398e664c7b4209c5f24ec2852e79c5af439f9b
DiegoLobera7 <diegolobera7@gmail.com>
2026-04-26 15:51:49 -0400

    Added CV

--- CapstoneTest/TestimonialsPage.aspx.designer.cs
+++ CapstoneTest/TestimonialsPage.aspx.designer.cs
@@ -60,6 +60,15 @@ namespace CapstoneTest
         protected global::System.Web.UI.WebControls.HyperLink HyperLink3;
 
         /// <summary>
+        /// HyperLink4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink4;
+
+        /// <summary>
         /// Panel1 control.
         /// </summary>
         /// <remarks>
